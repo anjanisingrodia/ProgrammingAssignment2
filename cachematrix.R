bd5b9993d1151d2f7f1ef7d014786e3635818ab6
Anjani K. Singrodia <anjanisingrodia@gmail.com>
2015-01-24 02:00:08 -0800

    the first function
    Have a working function for matrices using the concept of vector
    initiation.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    
+    m <- matrix()
+    set <- function(y) {
+        x <<- y
+        m <<- matrix()
+    }
+    get <- function() x
+    setinverse <- function(inv) m <<- inv
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 makeVector <- function(x = numeric()) {
